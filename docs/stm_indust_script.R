3e9149cc822fba300ddf52790e081dfbec8955bb
dnestor <dnestor@umass.edu>
2022-05-18 03:24:59 -0400

    Post 7

--- docs/stm_indust_script.R
+++ docs/stm_indust_script.R
@@ -1,15 +1,15 @@
 library(stm)
 
-stm_indust_3 <- stm(indust_trim_dfm, 
-                  K = 8, 
-                  prevalence = ~ employer + union_clean + location, 
-                  content = ~ union_clean, 
-                  data = clust_meta_indust, 
-                  init.type = "Spectral", 
-                  max.em.its = 75, 
-                  verbose = TRUE, 
-                  interactions = FALSE,
-                  gamma.prior = "L1")
+stm_man <- stm(man_trim_dfm, 
+               K = 8, 
+               prevalence = ~ employer + union_clean + location, 
+               content = ~ union_clean, 
+               data = clust_meta_man, 
+               init.type = "Spectral", 
+               max.em.its = 75, 
+               verbose = TRUE, 
+               interactions = FALSE,
+               gamma.prior = "L1")
 beepr::beep(8)
 
 
